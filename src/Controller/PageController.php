3d4935a5a7840238ac7707f0397fbcf9a238d89d
Roland Golla <rolandgolla@gmail.com>
2020-11-22 20:28:02 +0100

    Add prperty klaroItems to twig template in PageController.php Use klaroItems array in analytics.html.twig

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -87,14 +87,20 @@ class PageController extends WebsiteController
         StructureInterface $structure,
         WordpressService $wordpressService,
         MeetupService $meetupService,
+        Request $request,
         $preview = false,
         $partial = false
     ) {
+
+        $klaroCookie = $request->cookies->get('klaro');
+        $klaroItems = json_decode($klaroCookie, true);
+
         return $this->renderStructure(
             $structure,
             [
                 'blogList' => $wordpressService->getItemsFromBlog(),
-                'meetupNextEvents' => $meetupService->getNextEvents()
+                'meetupNextEvents' => $meetupService->getNextEvents(),
+                'klaroItems' => $klaroItems
             ],
             $preview,
             $partial
