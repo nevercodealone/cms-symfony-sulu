e9d3daeff4b5e5fa8a489936d0af7001aa245f99
Roland Golla <rolandgolla@gmail.com>
2021-05-28 01:40:30 +0200

    Remove skipped slider test in servicesCest.php

--- tests/acceptance/pageobjects/servicesCest.php
+++ tests/acceptance/pageobjects/servicesCest.php
@@ -29,7 +29,7 @@ class servicesCest
         $first = true;
         foreach ($contentItems as $contentItem) {
             // First item is visible
-            if($first) {
+            if ($first) {
                 $I->seeElement('#' . $contentItem);
                 $first = false;
                 continue;
@@ -37,40 +37,5 @@ class servicesCest
 
             $I->cantSeeElement('#' . $contentItem);
         }
-    }
-
-    /**
-     * @skip
-     */
-    public function onClickElementIsVisibleAndAllOthersNot(AcceptanceTester $I, startpage $startpage)
-    {
-        $I->scrollTo($startpage::$serviceSection);
-
-        $anchors = $I->grabMultiple($startpage::$servicesNavigation, 'href');
-        $contentItems = $I->grabMultiple($startpage::$servicesItems, 'id');
-
-        array_shift($anchors);
-        $firstItem = array_shift($contentItems);
-
-        $I->seeElement('#' . $firstItem);
-
-        foreach ($anchors as $key => $anchor)
-        {
-            $I->click('//*[@data-q="services-navigation"]/li[' . ($key + 2) . ']/div/a');
-            $I->waitForElementVisible('#' . $contentItems[$key]);
-
-            $actualContentItems = $I->grabMultiple($startpage::$servicesItems, 'id');
-            foreach ($actualContentItems as $actualContentItem) {
-                if($actualContentItem === $contentItems[$key]) {
-                    continue;
-                }
-
-                $I->cantSeeElement('#' . $actualContentItem);
-            }
-        }
-
-        $I->scrollTo($startpage::$serviceSection);
-        $I->click('//*[@data-q="services-navigation"]/li[1]/div/a');
-        $I->waitForElementVisible('#' . $firstItem);
     }
 }
