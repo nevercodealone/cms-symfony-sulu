e4830da26c2b2ff5301967236aef5d9c67ecb3bd
Roland Golla <rolandgolla@gmail.com>
2021-08-06 10:40:35 +0200

    Bring in 5 seconds break between each test with Acceptance helper _before method

--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -4,8 +4,16 @@ namespace NCATesting\Helper;
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
+use Codeception\TestInterface;
+
 class Acceptance extends \Codeception\Module
 {
+    public function _before(TestInterface $test)
+    {
+        sleep(5);
+        parent::_before($test);
+    }
+
     /**
      * https://stackoverflow.com/questions/26183792/use-codeception-assertion-in-conditional-if-statement
      * @param $element
