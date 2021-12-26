c133a4473feeadf7dec05ca6c1c2cefac36bc027
Roland <rolandgolla@gmail.com>
2021-12-26 22:27:42 +0100

    Add YouTube playlist content type
    Update blocks.xml and add new xml and twig file
    Add YouTubeTwigExtension class

---
+++ src/Twig/YoutubeTwigExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Twig;
+
+use App\Service\YouTubeService;
+use Google_Service_YouTube;
+use Symfony\Component\Intl\Countries;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+/**
+ * This is just an example Twig Extension.
+ * It is recommended to use `intl_country` from the `sulu/web-twig` package instead of this.
+ */
+class YoutubeTwigExtension extends AbstractExtension
+{
+    /**
+     * @var YouTubeService
+     */
+    private $youTubeService;
+
+    public function __construct(YouTubeService $youTubeService)
+    {
+        $this->youTubeService = $youTubeService;
+    }
+
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('youtube', [$this, 'playlistItemsListByPlaylistId']),
+        ];
+    }
+
+    public function playlistItemsListByPlaylistId(string $playlistId): array
+    {
+        return $this->youTubeService->getItemsFromChannel($playlistId);
+    }
+}
