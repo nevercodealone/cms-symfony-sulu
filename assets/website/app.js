cac564994c522d89b5a4419c9f065c145a09e5fe
Roland <rolandgolla@gmail.com>
2024-10-14 19:03:04 +0200

    Rebuild tailwind.

--- assets/website/app.js
+++ assets/website/app.js
@@ -1,18 +1,24 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
-
-// start the Stimulus application
 import './bootstrap';
-
 import Alpine from 'alpinejs'
 
 window.Alpine = Alpine
+
+Alpine.data('clipboardBlock', () => ({
+  copied: false,
+  copyCode() {
+    const codeBlock = this.$refs.codeBlock;
+    const codeText = codeBlock.textContent;
+
+    navigator.clipboard.writeText(codeText).then(() => {
+      this.copied = true;
+      setTimeout(() => {
+        this.copied = false;
+      }, 2000);
+    }).catch(err => {
+      console.error('Failed to copy text: ', err);
+    });
+  }
+}))
 
 Alpine.start()
