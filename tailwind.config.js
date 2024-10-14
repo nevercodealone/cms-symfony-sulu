4fcac5ec754866eb40023aaf5074cd9ca97e5567
Roland <rolandgolla@gmail.com>
2024-10-14 13:24:46 +0200

    Add default listStyleType to tailwind.config.js.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,45 @@ module.exports = {
     "./templates/includes/tailwind/blocks/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      listStyleType: {
+        none: "none",
+        disc: "disc",
+        decimal: "decimal",
+        square: "square",
+        roman: "upper-roman",
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
     require("daisyui"),
+    function({ addBase, theme }) {
+      addBase({
+        'ul': {
+          listStyleType: theme('listStyleType.disc'),
+          paddingLeft: theme('spacing.6'),
+        },
+        'ol': {
+          listStyleType: theme('listStyleType.decimal'),
+          paddingLeft: theme('spacing.6'),
+        },
+        'ul ul, ol ul': {
+          listStyleType: theme('listStyleType.circle'),
+          marginTop: theme('spacing.1'),
+          marginBottom: theme('spacing.1'),
+        },
+        'ol ol, ul ol': {
+          listStyleType: theme('listStyleType.decimal'),
+          marginTop: theme('spacing.1'),
+          marginBottom: theme('spacing.1'),
+        },
+        'li': {
+          marginTop: theme('spacing.1'),
+          marginBottom: theme('spacing.1'),
+        }
+      })
+    },
   ],
   daisyui: {
     themes: ["light", "dark", "cupcake"],
