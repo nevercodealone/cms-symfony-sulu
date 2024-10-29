7edbb9ce9245ca61ea5d1b721b6bec4333319719
Roland <rolandgolla@gmail.com>
2024-10-29 22:41:34 +0100

    Finish task.

--- migrations/Version20241029214037.php
+++ migrations/Version20241029214037.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241029213120 extends AbstractMigration
+final class Version20241029214037 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20241029213120 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE client_request_refactoring CHANGE contact_type contact_type VARCHAR(20) DEFAULT NULL');
+        $this->addSql('CREATE TABLE client_request_refactoring (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, project_url VARCHAR(255) DEFAULT NULL, project_description LONGTEXT NOT NULL, team VARCHAR(20) NOT NULL, contact_type VARCHAR(20) DEFAULT NULL, additional_information LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE client_request_refactoring CHANGE contact_type contact_type VARCHAR(20) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('DROP TABLE client_request_refactoring');
     }
 }
